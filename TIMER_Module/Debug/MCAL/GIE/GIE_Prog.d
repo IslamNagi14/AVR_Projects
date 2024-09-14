7a97990e5d0b5ec6ec4dceafd1573baa9abb5b4b
Islam Nagi <152648172+IslamNagi14@users.noreply.github.com>
2024-09-14 21:33:48 +0300

    Delete TIMER_Module directory

--- TIMER_Module/Debug/MCAL/GIE/GIE_Prog.d
+++
@@ -1,9 +0,0 @@
-MCAL/GIE/GIE_Prog.o MCAL/GIE/GIE_Prog.o: ../MCAL/GIE/GIE_Prog.c \
-  ../MCAL/GIE/../../LIB/StdTypes.h ../MCAL/GIE/../../LIB/errorStatuse.h \
-  ../MCAL/GIE/GIE_Priv.h
-
-../MCAL/GIE/../../LIB/StdTypes.h:
-
-../MCAL/GIE/../../LIB/errorStatuse.h:
-
-../MCAL/GIE/GIE_Priv.h:
